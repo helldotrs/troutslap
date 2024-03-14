937138efd69979afda2be017067722ca600023dd
helldotrs <macrosystem@proton.me>
2024-03-14 16:17:28 +0100

    completed. for now.

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import argparse
 
-source_var = "admin"
-target_var = "them selves"
-slap_string = f"{source_var} slaps {target_var} around a bit with a large trout"
-rtfm_var = """rtfm:
---host host-address
---target target-address
---output file-name (optional)
-"""
 
-parser = argparse.ArgumentParser(description="slap target around a bit with a large trout")
+
+parser = argparse.ArgumentParser(description="test-tool")
 
 parser.add_argument("-t", "--target", type=str, help="Target address")
 parser.add_argument("-s", "--source", type=str, help="Source address")
@@ -17,14 +10,14 @@ parser.add_argument("-o", "--output", type=str, help="Output file name (optional
 
 args = parser.parse_args()
 
-
-
-if args.target:
-    target_var = args.target
-
-if args.source:
-    source_var = args.source
-
+source_var  = "Admin"           if args.source is None else args.source
+target_var  = "them seleves"    if args.target is None else args.target
+slap_string = f"{source_var} slaps {target_var} around a bit with a large trout"
+rtfm_var    = """rtfm:
+--host host-address
+--target target-address
+--output file-name (optional)
+"""
 
 if not args.output: #False being default state
     print(slap_string)
